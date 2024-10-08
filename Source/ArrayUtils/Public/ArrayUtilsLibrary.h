8ac8c6c54d9c79c8d7bd4feff27588e1ab96d61f
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2024-10-08 14:34:50 +0900

    add comment for GenericAdjacentFind

--- Source/ArrayUtils/Public/ArrayUtilsLibrary.h
+++ Source/ArrayUtils/Public/ArrayUtilsLibrary.h
@@ -110,6 +110,22 @@ public:
 	                         const FName&               ComparisonFunctionName);
 
 public:
+	/**
+	 * Searches for the first pair of adjacent elements that satisfy the
+	 * condition. Returns the index of the first element of such a pair.
+	 * @param TargetArray  target array
+	 * @param ArrayProperty  property of TargetArray
+	 * @param BinaryPredicate
+	 *    A binary predicate function that defines whether the pair of
+	 *    adjacent elements satisfies the condition. This must be a function that
+	 *    has two arguments of the same type as the array elements and returns a
+	 *    bool. If the pair is considered to meet your intended condition,
+	 *    return true; otherwise, return false.
+	 * @return
+	 *    Returns the index of the first element of the first pair of adjacent
+	 *    elements such that BinaryPredicate returns true. If not found, return
+	 *    the number of elements in the array (means out of index).
+	 */
 	static int32 GenericAdjacentFind(const void*           TargetArray,
 	                                 const FArrayProperty& ArrayProperty,
 	                                 UFunction&            BinaryPredicate);
